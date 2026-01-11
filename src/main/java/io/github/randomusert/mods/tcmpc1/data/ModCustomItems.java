c3c0b05d2f35b0b2bcd457e853d2f4cd5832ad6e
randomusert <eetupajamaki@gmail.com>
2026-01-11 12:45:14 +0200

    test recipe for mekanism basic energy cube

---
+++ src/main/java/io/github/randomusert/mods/tcmpc1/data/ModCustomItems.java
@@ -0,0 +1,9 @@
+package io.github.randomusert.mods.tcmpc1.data;
+
+import io.github.randomusert.mods.tcmpc1.common.util.ItemStringToItemLike;
+import net.minecraft.world.level.ItemLike;
+
+// this class stores variable for items outside of this mod!! DO NOT DELETE!!!!
+public class ModCustomItems {
+    public static ItemLike BASIC_ENERGY_CUBE_MEK = ItemStringToItemLike.ItemStringToItemLike("mekanism", "basic_energy_cube");
+}
--- src/main/java/io/github/randomusert/mods/tcmpc1/data/ModRecipeProvider.java
+++ src/main/java/io/github/randomusert/mods/tcmpc1/data/ModRecipeProvider.java
@@ -4,17 +4,26 @@ import java.util.concurrent.CompletableFuture;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.ShapedRecipe;
 
 public class ModRecipeProvider extends RecipeProvider {
     public ModRecipeProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> registries) {
         super(packOutput, registries);
     }
 
+
+
     @Override
     protected void buildRecipes(RecipeOutput output) {
-
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModCustomItems.BASIC_ENERGY_CUBE_MEK)
+                .requires(Items.IRON_BLOCK, 9)
+                .unlockedBy("has_iron_block", has(Items.IRON_BLOCK))
+                .save(output);
     }
 
 }
